22dc6fe8f60bcac0f985f9fa0bc8ad934e83783d
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2020-08-27 21:24:35 +0530

    Created All Required Components

---
+++ src/app/new-listing-page/new-listing-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-new-listing-page',
+  templateUrl: './new-listing-page.component.html',
+  styleUrls: ['./new-listing-page.component.scss']
+})
+export class NewListingPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
