93fd74f835b540d044b834d0fec7f67a6fa71b7f
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2020-08-28 22:26:15 +0530

    Added Basic Listing Detail Page

--- src/app/listing-detail-page/listing-detail-page.component.ts
+++ src/app/listing-detail-page/listing-detail-page.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {fakeListings} from '../fake-data';
+import {Listing} from '../types';
 
 @Component({
   selector: 'app-listing-detail-page',
@@ -7,9 +10,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListingDetailPageComponent implements OnInit {
 
-  constructor() { }
+  listing: Listing;
+  constructor(private  route: ActivatedRoute) {
+
+  }
 
   ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    this.listing = fakeListings.find(listing => listing.id === id);
   }
 
 }
