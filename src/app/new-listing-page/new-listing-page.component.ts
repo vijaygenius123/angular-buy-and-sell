b23b2e5981602e6c8259e50add856dda7d9e4c01
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2020-08-29 20:44:33 +0530

    Added New Listing Page

--- src/app/new-listing-page/new-listing-page.component.ts
+++ src/app/new-listing-page/new-listing-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-new-listing-page',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewListingPageComponent implements OnInit {
 
-  constructor() { }
+  name = '';
+  description = '';
+  price = 0;
+
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(): void {
+    alert('Creating new listing');
+    this.router.navigateByUrl('/my-listings');
   }
 
 }
